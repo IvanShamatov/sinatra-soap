c76953028c9ff9ae030c721fc100aab126d63804
Ivan Shamatov <status.enable@gmail.com>
2013-11-25 17:51:57 +0400

    initial commit

---
+++ lib/sinatra/soap.rb
@@ -0,0 +1,7 @@
+require "sinatra/soap/version"
+
+module Sinatra
+  module Soap
+    # Your code goes here...
+  end
+end
---
+++ lib/sinatra/soap/version.rb
@@ -0,0 +1,5 @@
+module Sinatra
+  module Soap
+    VERSION = "0.0.1"
+  end
+end
